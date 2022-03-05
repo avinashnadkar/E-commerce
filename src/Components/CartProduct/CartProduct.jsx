78dad602d6a61cc82c1d7dd24cf54c2d9200a008
avinashnadkar <avinashnadkar3@gmail.com>
2022-03-05 20:06:11 +0530

    created quantity counter layout in cart product

--- src/Components/CartProduct/CartProduct.jsx
+++ src/Components/CartProduct/CartProduct.jsx
@@ -3,14 +3,21 @@ import styles from "./CartProduct.module.css";
 const CartProduct = (props) => {
     return(
        <div className={styles.cartProduct}>
-           <img src={props.img}/>
-           <div className={styles.productDetails}>
-               <h3 className={styles.productName}>{props.name}</h3>
-               <div className={styles.priceDetails}>
-                  <h4>₹{props.price - props.discount}.00</h4>
-                  <del><p>₹{props.price}.00</p></del>
-                  <p className={styles.saving}>You Save ₹{props.discount}.00</p>
-               </div>
+           <div className={styles.cartProductContainer}>
+                <img src={props.img}/>
+                <div className={styles.productDetails}>
+                    <h3 className={styles.productName}>{props.name}</h3>
+                    <div className={styles.priceDetails}>
+                        <h4>₹{props.price - props.discount}.00</h4>
+                        <del><p>₹{props.price}.00</p></del>
+                        <p className={styles.saving}>You Save ₹{props.discount}.00</p>
+                    </div>
+                </div>
+           </div>
+           <div className={styles.quantityCounter}>
+               <button>-</button>
+               {props.quantity}
+               <button>+</button>
            </div>
        </div>
     )

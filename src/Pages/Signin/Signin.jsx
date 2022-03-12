c2192e0074fcf65a487d2822e7e81cb5a269c10a
avinashnadkar <avinashnadkar3@gmail.com>
2022-03-12 23:00:17 +0530

    changed sign in page state managment to redux

--- src/Pages/Signin/Signin.jsx
+++ src/Pages/Signin/Signin.jsx
@@ -1,32 +1,18 @@
 import styles from "./Signin.module.css";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { useState } from "react";
+import { useSelector, useDispatch  } from "react-redux";
+import {loginInputHandler,otpInputHandler,otpInputToggle} from "../../Redux/action";
 
 const Signin = () => {
 
-    //state for mobile number
-    const [mobileNumber, setMobileNumber] = useState("");
-    //state for otp
-    const [otp, setOtp] = useState("");
-    //state for to check phone number submited or not submited
-    const [submited, setSubmited] = useState(false);
 
-    //handle state change of phone number
-    const handleChange = (e) => {
-        setMobileNumber(e.target.value);
-        console.log(mobileNumber);
-    }
+    //States from store
+    const phoneNumber = useSelector((state)=>state.phoneNumber);
+    const otp = useSelector((state)=>state.otp);
+    const showOtpBox = useSelector((state)=>state.showOtpInputBox);
 
-    //change state change of otp
-    const handleChangeOtp = (e) => {
-        setOtp(e.target.value);
-        console.log(otp);
-    }
-
-    //Submit phone number
-    const handleSubmit = () => {
-        setSubmited(true);
-    }
+    //dispatch function to dispatch actions to reducer
+    const dispatch = useDispatch();
 
     return (
         <div>
@@ -35,13 +21,13 @@ const Signin = () => {
                <p>Sign in to access your Orders, Offers and Wishlist.</p>
                <div className={styles.inputBox}>
                   <span className={styles.countryCode}>+91</span>
-                  <input type="text" className={styles.mobileNumInput} placeholder="Enter your mobile number" value={mobileNumber} onChange={handleChange}/>
+                  <input type="text" className={styles.mobileNumInput} placeholder="Enter your mobile number" value={phoneNumber} onChange={(e)=>dispatch(loginInputHandler(e.target.value))}/>
                </div>
-               <button className={styles.submitBtn} style={{display : submited ?  "none" : "block" }} onClick={handleSubmit}><ArrowForwardIosIcon/></button>
+               <button className={styles.submitBtn} style={{display : showOtpBox ?  "none" : "block" }} onClick={()=>dispatch(otpInputToggle())}><ArrowForwardIosIcon/></button>
 
-               <div style={{display : submited ? "block" : "none"}}>
+               <div style={{display : showOtpBox  ? "block" : "none"}}>
                     <div className={styles.inputBoxOtp}>
-                        <input type="text" className={styles.otpInput} placeholder="Enter your OTP" value={otp} onChange={handleChangeOtp}/>
+                        <input type="text" className={styles.otpInput} placeholder="Enter your OTP" value={otp} onChange={(e)=>dispatch(otpInputHandler(e.target.value))}/>
                     </div>
 
                     <button className={styles.verifyBtn}>Verify</button>
